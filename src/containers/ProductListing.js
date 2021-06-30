18bea3b1ad9e71e736ac8a120d2a92d382dcbc1f
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-06-30 05:05:35 -0700

    add fake api

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,9 +1,30 @@
-import React from "react"
+import React, {useEffect} from "react"
+import axios from "axios"
+import { useSelector } from "react-redux"
+import ProductComponent from "./ProductCompunent"
 
 const ProductListing = () => {
+    const products = useSelector((state) => state)
+
+
+    const fetchProducts = async() => {
+        const response = await axios
+        .get("https://fakestoreapi.com/products")
+        .catch((error) => {
+            console.log(error)
+        })
+
+        console.log(response)
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    console.log(products)
     return (
-        <div>
-            <h1>ProductListing</h1>
+        <div className="ui grid container">
+            <ProductComponent/>
         </div>
     )
 }
